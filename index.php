cc908910308f0181c481706c79e79ab011d351ca
ts44gj <ts44gj2020@gmail.com>
2020-11-09 18:11:48 +0900

    エラーチェックでタイトルの文字数と記事の入力チェックを追加

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
 $title=""; //タイトルの変数
 $text=""; //テキストの変数
+$ERRO=array();//エラーを確認するための変数
 
 $FILE = "article.txt"; //保存ファイル名
 $id = uniqid(); //ユニークなIDを自動生成
 $DATE = []; //一回分の投稿の情報を入れる
 $BOARD = []; //全ての投稿の情報を入れる
-$ERRO = array();
+
+
 
 //$FILEというファイルが存在する時
 if (file_exists($FILE)){
   //ファイルを読み込む
 $BOARD = json_decode(file_get_contents($FILE));
+
 }
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
   //リクエストパラメータがからでなければ
-if(!empty($_POST["txt"]) && !empty($_POST["title"])){
+if(!empty($_POST["text"]) && !empty($_POST["title"])){
    //投稿ボタンが押された時
 
    //$textに送信されたテキストを送信
  $title=$_POST["title"];
- $text=$_POST["txt"];
+ $text=$_POST["text"];
   //この後に保存の処理をする
   //新規データ
   $DATE=[$id,$title,$text];
@@ -29,11 +32,20 @@ if(!empty($_POST["txt"]) && !empty($_POST["title"])){
   //全体配列をファイルに保存する
   file_put_contents($FILE, json_encode($BOARD)); 
 
-  //この下に文字が入力されてない時の反応を入力する
-if(empty($_POST["title"])){
-    $ERRO[]="タイトルが入力されていません";
-  }
  }
+}
+//この下に入力チェックを記入する
+  //タイトル
+if(empty($_POST["title"])){
+  $ERRO[]="タイトルを入力してください";
+}
+  //タイトルの文字制限
+if(mb_strlen($_POSR["title"])>30){
+  $ERRO[]="タイトルは30文字以内で入力してください";
+}
+  //記事
+if(empty($POST["text"])){
+  $ERRO[]="記事を入力してください";
 }
 
 ?>
@@ -47,12 +59,7 @@ if(empty($_POST["title"])){
  </head>
 
  <body>
-   <!--メッセージの表示-->
-    <ul>
-      <?php foreach($ERRO as $value): ?>
-      <li><?php echo $value; ?></li>
-      <?php endforeach;?>
-      </ul>
+
 
  <!--確認ダイアログを表示するための関数-->
  <script>
@@ -64,6 +71,12 @@ if(empty($_POST["title"])){
  </script>
 
   <h1>larabalnews</h1>
+     <!--エラーメッセージの表示-->
+     <ul>
+      <?php foreach($ERRO as $erro_message): ?>
+      <li><?php echo $erro_message; ?></li>
+      <?php endforeach;?>
+      </ul>
 
   <!--投稿-->
    <form id="push"  method="POST" name="lalavel news"  onsubmit="return dialog()"> 
@@ -73,7 +86,7 @@ if(empty($_POST["title"])){
       </div>
       <div>
           <p>記事</p>
-          <textarea row="10" cols="60" name="txt"> </textarea>
+          <textarea row="10" cols="60" name="text"> </textarea>
       </div>
       <div>
           <input type="submit" name="push" value="投稿"　onclick="alert()">

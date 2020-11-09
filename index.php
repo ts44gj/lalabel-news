1c96929c64674707bcba847d0ff598045dce121a
ts44gj <ts44gj2020@gmail.com>
2020-11-09 14:25:02 +0900

    a

--- index.php
+++ index.php
@@ -1,56 +1,88 @@
 <?php
-$title="";
-$kiji="";
+$title=""; //タイトルの変数
+$text=""; //テキストの変数
 
-/*$FILE = `article.text`;
-$id = uniqid();
-$DATE = [];
-$BOARD + [];*/
+$FILE = "article.txt"; //保存ファイル名
+$id = uniqid(); //ユニークなIDを自動生成
+$DATE = []; //一回分の投稿の情報を入れる
+$BOARD = []; //全ての投稿の情報を入れる
+$ERROR_MESSAGE = array();
 
-/*if (file_exists($FILE)){
- $BOARD = json_decode(file_get_contents($FILE));*/
-
+//$FILEというファイルが存在する時
+if (file_exists($FILE)){
+  //ファイルを読み込む
+$BOARD = json_decode(file_get_contents($FILE));
+}
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
+  //リクエストパラメータがからでなければ
+if(!empty($_POST["txt"]) && !empty($_POST["title"])){
+   //投稿ボタンが押された時
 
- if(!empty($_POST["kiji"]) && !empty($_POST["title"])){
-  $title=$_POST["title"];
-  $kiji=$_POST["kiji"];
+   //$textに送信されたテキストを送信
+ $title=$_POST["title"];
+ $text=$_POST["txt"];
+  //この後に保存の処理をする
+  //新規データ
+  $DATE=[$id,$title,$text];
+  $BOARD[] = $DATE;
   
-  /*$DATE=[$id,$title,$text];
-  $BOARD[] = $DATE;*/
-  
+  //全体配列をファイルに保存する
   file_put_contents($FILE, json_encode($BOARD)); 
  }
-}
+ }
+
 ?>
 <!DOCTYPE html>
-<html>
+ <html>
 
-<head>
+ <head>
   <meta charset='utf-8'>
-  <title>lalabelnews<</title>
-</head>
+  <title>larabelnews<</title>
+  <!--<link rel="stylesheet" href="stylesheet.css">-->
+ </head>
 
-<body>
-   <form  method="POST" name="lalavel news" > 
-       <div>
-       <input type="text" name="title" >
+ <body>
+
+ <!--確認ダイアログを表示するための関数-->
+ <script>
+  function dialog(){
+    let popup =confirm("入力に間違いはないですか？")
+
+    return popup;
+    }　
+ </script>
+
+  <!--投稿-->
+   <form id="push"  method="POST" name="lalavel news"  onsubmit="return dialog()"> 
+      <div>
+         <p>タイトル</p>
+          <input type="text" name="title" >
       </div>
       <div>
-       <textarea row="10" cols="60" name="kiji"> </textarea>
+          <p>記事</p>
+          <textarea row="10" cols="60" name="txt"> </textarea>
       </div>
-       <input type="submit" name="sousin">
+      <div>
+          <input type="submit" name="push" value="投稿"　onclick="alert()">
+      </div>
    </form>
 
-    <h3>
-     <?php echo $title ?>
-    </h3>
-    <p>
-    <?php echo $kiji ?>
-    </p>
-<div>
+  <!--コメント-->
+  <hr>
+  <div>
+     <!--$BOARDから$ARTICLEへ移行？-->
+      <?php foreach ((array)$BOARD as $ARTICLE)  : ?>
+  </div>
+  <p>
+      <?php echo $ARTICLE[1];?>
+  </p>
+  <p>
+      <?php echo $ARTICLE[2];?>
+  </p>
+  <div>
+    <?php endforeach; ?>
+  </div>
 
-
-</body>
+ </body>
 
 </html>

2cce95d7f07ac2f72739ed49058ce24620453f92
ts44gj <ts44gj2020@gmail.com>
2020-11-09 16:27:31 +0900

    エラーメッセージ加筆

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $FILE = "article.txt"; //保存ファイル名
 $id = uniqid(); //ユニークなIDを自動生成
 $DATE = []; //一回分の投稿の情報を入れる
 $BOARD = []; //全ての投稿の情報を入れる
-$ERROR_MESSAGE = array();
+$ERRO = array();
 
 //$FILEというファイルが存在する時
 if (file_exists($FILE)){
@@ -28,8 +28,13 @@ if(!empty($_POST["txt"]) && !empty($_POST["title"])){
   
   //全体配列をファイルに保存する
   file_put_contents($FILE, json_encode($BOARD)); 
+
+  //この下に文字が入力されてない時の反応を入力する
+if(empty($_POST["title"])){
+    $ERRO[]="タイトルが入力されていません";
+  }
  }
- }
+}
 
 ?>
 <!DOCTYPE html>
@@ -42,6 +47,12 @@ if(!empty($_POST["txt"]) && !empty($_POST["title"])){
  </head>
 
  <body>
+   <!--メッセージの表示-->
+    <ul>
+      <?php foreach($ERRO as $value): ?>
+      <li><?php echo $value; ?></li>
+      <?php endforeach;?>
+      </ul>
 
  <!--確認ダイアログを表示するための関数-->
  <script>
@@ -51,6 +62,8 @@ if(!empty($_POST["txt"]) && !empty($_POST["title"])){
     return popup;
     }　
  </script>
+
+  <h1>larabalnews</h1>
 
   <!--投稿-->
    <form id="push"  method="POST" name="lalavel news"  onsubmit="return dialog()"> 

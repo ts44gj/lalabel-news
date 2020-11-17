7d6f5d339e996fda261372e6bc1133d278c55edf
ts44gj <ts44gj2020@gmail.com>
2020-11-18 04:18:47 +0900

    実験中

--- dbc.php
+++ dbc.php
@@ -1,10 +1,39 @@
 <?php
+//変数を用意
+$dsn = "mysql:host=localhost;dbname=laravel_news;charset=utf8"; //DBの場所、名前
+$user = "ts44gj"; //DBのユーザ名
+$pass = "ts44gj"; //DBのパスワード
 
-$dsn = "mysql:host=localhost;dbname=laravel_news;charset=utf8";
-$user = "ts44gj";
-$pass = "ts44gj";
+//文字化け防止
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET 'utf8'");
 
-$dbh = new PDO ($dsn,$user,$pass);
-var_dump($dbh);
+//PHPのエラーを表示する
+error_reporting(E_ALL &~E_NOTICE);
+
+//DB接続　setAttributeからエラー表示
+try {
+    $dbh = new PDO ($dsn,$user,$pass,$options);
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo $e->getMessage();
+    exit;
+}
+
+$dbh->query("INSERT INTO date_table VALUES (4444, 5555,6666)",);
+
+// SQL文とその中のパラメータを変数にすることもできる
+//$sql = 'SELECT  '商品', '価格' WHERE '商品ID' = $shohin_id';
+//$dbh->query($sql);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
--- task.php
+++ task.php
@@ -9,16 +9,25 @@ $DATE = []; //一回分の投稿の情報を入れる
 $BOARD = []; //全ての投稿の情報を入れる
 $x=$_GET["$id"]; //$_getでidを獲得
 
-//mysqlに接続
+//変数を用意
+$dsn = "mysql:host=localhost;dbname=laravel_news;charset=utf8"; //DBの場所、名前
+$user = "ts44gj"; //DBのユーザ名
+$pass = "ts44gj"; //DBのパスワード
 
-//mysqlに接続する
-$dsn = "mysql:host=localhost;dbname=laravel_news;charset=utf8";
-$user = "ts44gj";
-$pass = "ts44gj";
+//文字化け防止
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET 'utf8'");
 
+//PHPのエラーを表示する
+error_reporting(E_ALL &~E_NOTICE);
 
-$dbh = new PDO ($dsn,$user,$pass,[
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,]);
+//DB接続　setAttributeからエラー表示
+try {
+    $dbh = new PDO ($dsn,$user,$pass,$options);
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo $e->getMessage();
+    exit;
+}
 
 
 // $FILEというファイルが存在する時
